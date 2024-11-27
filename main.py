4d542387c48a95cb4554f9107d19a86607d2b904
managepeep <magnetic.turtle.vprb@instantletter.net>
2024-11-27 14:10:38 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
-import requests
+def getDict(file):
+	words = set()
+	with open(file) as d:
+		for w in d.read().split("\n"):
+			words.add(w.lower())
+	return words
+
+def isEng(word):
+	englishWords = getDict("dictionary.txt")
+	if word in englishWords:
+		return True
+	return False
+
+def permutations(xl, length = -1, res=[], output=[]):
+	if xl == [] or len(res) == length:
+		output.append(res)
+		return
+	for i in range(len(xl)):
+		permutations(xl[:i] + xl[i + 1:], length, res + [xl[i]], output)
+	return output
 
 
-def get_temperature(json_data):
-    temp_in_celcius = json_data['main']['temp']
-    return temp_in_celcius
-
-def get_weather_type(json_data):
-    weather_type = json_data['weather'][0]['description']
-    return weather_type
-
-def get_wind_speed(json_data):
-    wind_speed = json_data['wind']['speed']
-    return wind_speed
-
-
-
-def get_weather_data(json_data, city):
-    description_of_weather = json_data['weather'][0]['description']
-    weather_type = get_weather_type(json_data)
-    temperature = get_temperature(json_data)
-    wind_speed = get_wind_speed(json_data)
-    weather_details = ''
-    return weather_details + ("The weather in {} is currently {} with a temperature of {} degrees and wind speeds reaching {} km/ph".format(city, weather_type, temperature, wind_speed))
-
-
-def main():
-    api_address = 'https://api.openweathermap.org/data/2.5/weather?q=Sydney,au&appid=a10fd8a212e47edf8d946f26fb4cdef8&q='
-    city = input("City Name : ")
-    units_format = "&units=metric"
-    final_url = api_address + city + units_format
-    json_data = requests.get(final_url).json()
-    weather_details = get_weather_data(json_data, city)
-    # print formatted data
-    print(weather_details)
-
-
-
-main()
+while True:
+	found = set()
+	letters = [i for i in input("Choose letters: ")]
+	for sz in range(2, len(letters)+1):
+		print("\nSize:", sz, "letters")
+		for comb in permutations(letters, sz ,[], []):
+			if isEng("".join(comb)) and not "".join(comb) in found:
+				print("Found word:", "".join(comb))
+				found.add("".join(comb))
+	print()
+	
